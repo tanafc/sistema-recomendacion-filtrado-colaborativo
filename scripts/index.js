a23ad5c0261cba6113a257db624fb138e8cda6d4
Tana <tana.fcasanova@gmail.com>
2022-10-21 12:16:33 +0100

    Añadido campos de ingreso de datos

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton =  document.getElementById('doButton');
+    const matrixFile = document.getElementById('inputMatrixToRead');
+    
+    function readMatrix() {
+
+    }
+
+    // function calculatePrediction() {
+    //     const matrix = matrixFile.files[0];
+    //     const fr = new FileReader();
+    //     fr.readAsText(matrix);
+    //     fr.onload = function () {
+    //         console.log(fr.result);
+    //     };
+    // }
+
+    // startButton.addEventListener("click", calculatePrediction());
+});
